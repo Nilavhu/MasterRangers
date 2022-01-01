3196b277eb94cc4ef22ffeec999b144c432f5fcc
nilnilnilnil12321 <74089182+nilnilnilnil12321@users.noreply.github.com>
2022-01-01 11:52:20 +0400

    Update main.js

--- main.js
+++ main.js
@@ -1,66 +1,53 @@
-var canvas = new fabric.Canvas('myCanvas')
+canvas=new fabric.Canvas("myCanvas");
 
  block_y=1;
  block_x=1;
 
-block_image_width = 350;
-block_image_height = 430;
+block_width = 350;
+block_height = 430;
 
 var block_image_object= "";
 
-function new_image(get_image)
-{
-	fabric.Image.fromURL(get_image, function(Img){
-		block_image_object = Img;
-
-		block_image_object.scaleToWidth(block_image_width);
-		block_image_object.scaleToHeight(block_image_height);
-		block_image_object.set({
-			top:block_y,
-			left:block_x
-		});
-		canvas.add(block_image_object);
-	});
+function ranger_image(get_image){
+fabric.Image.fromURL(get_image,function(Img){
+block_image_object=Img;
+block_image_object.scaleToWidth(block_width);
+block_image_object.scaleToHeight(block_height);
+block_image_object.set({
+top:block_y,
+left:block_x
+});
+	canvas.add(block_image_object);
+});
 }
 
 window.addEventListener("keydown", my_keydown);
 
-function my_keydown(e)
-{
+function my_keydown(e){
 keyPressed = e.keyCode;
 console.log(keyPressed);
-
-	if(keyPressed == '82') 
-	{
-		block_x = 10
-		new_image('rr1.png');
-		console.log("r = red ranger");
-	}
-	if(keyPressed == '71')
-	{
-		block_x = 255;
-		new_image('gr.png');
-		console.log("g = green ranger");
-	}
-	
-	if(keyPressed == '89')
-	{
-		block_x =380;
-		new_image('yr.png');
-		console.log("y = yellow ranger");
-	}
-	if(keyPressed == '80')
-	{
-		block_x = 600;
-		new_image('pr.png');
-		console.log("p = pink ranger");
-	}
-	if(keyPressed == '66')
-	{
-		block_x = 680;
-		new_image('br.png');
-		console.log("b = blue ranger");
-	}
-	
+if(keyPressed == '82') {
+console.log("r");
+ranger_image("rr1.png")
 }
+if(keyPressed == '71'){
+block_x = 250;
+ranger_image("gr.png")
+}
+if(keyPressed == '89'){
+block_x =405;
+block_y=5
+ranger_image("yr.png")
+}
+if(keyPressed == '80'){
+block_x = 670;
+block_y=1;
+ranger_image("pr.png")
+}
+if(keyPressed == '66'){
+block_x = 795;
+block_y=-9;
+ranger_image("br.png")
+}
+	}
 
